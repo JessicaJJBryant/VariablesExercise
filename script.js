66617d313116f911ac7b3de6bfbcfb2896e66e78
Jessica Bryant <jessica.bryant@live.com>
2021-09-21 16:59:24 -0400

    Complete Variables Exercise with Bonuses

---
+++ script.js
@@ -0,0 +1,36 @@
+// VARIABLES EXERCISE
+
+// 1
+// const
+
+// 2
+let num = 20;
+// 3
+num = 15; 
+// 4
+num += 7;
+// 5
+num --;
+// 6
+num = "This is not a number";
+// 7
+let blank;
+// 8
+console.log(blank);
+// 9
+blank = null;
+// Bonus 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// Bonus 2
+console.log(quarter, dime, nickel, penny);
+// Bonus 3
+let letters = "qwertyuiop";
+// Bonus 4
+let length = letters.length - 1;
+console.log(letters[length]);
+/* OR
+console.log(letters.length); then run the node script.js to get 10 and subtract 1 for the 0. Then:
+console.log(letters[9]);    then run the node script.js to get the p
+OR
+You might be looking for an array. I looked this up online but I don't understand it yet (and got some errors when I tried it), so I guess I'll leave this alone for now. :-)
+*/
